a243ad3e2b8fda5fc7144fe91bf072055a9a425e
Ty-Pope <bobk12l04@gmail.com>
2023-10-07 13:07:32 -0700

    js creation

--- createProject.js
+++ createProject.js
@@ -1,3 +1,26 @@
-function click() {
- console.log("Hello!");
+function submitClick() {
+ let name = document.getElementById("project-name").value;
+ let description = document.getElementById("description").value;
+
+ let level = [false, false, false];
+ level[0] = document.getElementById("beginner").checked;
+ level[1] = document.getElementById("intermediate").checked;
+ level[2] = document.getElementById("advanced").checked;
+
+ let requirments = document.getElementById("skills-requirment").value;
+ requirments = requirments.toString();
+ requirments = requirments.split(",");
+ for (let i = 0; i < requirments.length; i++) {
+  console.log(requirments[i][0]);
+  let endIndex = requirments[i].length;
+  if (requirments[i][0] == " ") {
+   console.log("space");
+   requirments[i] = requirments[i].slice(1);
+  }
+  if (requirments[i][endIndex - 2] == " ") {
+   requirments[i] = requirments[i].slice(0, requirments[i].length - 1);
+  }
+ }
+
+ console.log(uuid);
 }
