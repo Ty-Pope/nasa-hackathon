4315280c570b5d128f93912f3ffdccb9887745f5
Ty-Pope <bobk12l04@gmail.com>
2023-10-07 18:15:01 -0700

    stuff

---
+++ public/myProjectRead.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+ const app = firebase.app();
+
+ const db = firebase.firestore();
+
+ const myPost = db.collection("posts").doc("testPost");
+ console.log(myPost);
+ myPost.get().then((doc) => {
+  const data = doc.data();
+  document.getElementById("data").innerHTML = data.name;
+ });
+});
