89bbc391cd89aff6fb8e63332fc46e08a203f595
Ty-Pope <bobk12l04@gmail.com>
2023-10-07 22:36:42 -0700

    login

---
+++ public/login.js
@@ -0,0 +1,28 @@
+function signIn() {
+ var provider = new firebase.auth.GoogleAuthProvider();
+
+ firebase
+  .auth()
+  .signInWithPopup(provider)
+  .then((result) => {
+   /** @type {firebase.auth.OAuthCredential} */
+   var credential = result.credential;
+
+   // This gives you a Google Access Token. You can use it to access the Google API.
+   var token = credential.accessToken;
+   // The signed-in user info.
+   var user = result.user;
+   // IdP data available in result.additionalUserInfo.profile.
+   document.getElementById("username").innerHTML = `Hello! ${user.displayName}`;
+  })
+  .catch((error) => {
+   // Handle Errors here.
+   var errorCode = error.code;
+   var errorMessage = error.message;
+   // The email of the user's account used.
+   var email = error.email;
+   // The firebase.auth.AuthCredential type that was used.
+   var credential = error.credential;
+   document.getElementById("username").innerHTML = `An error occured. Please try again later.`;
+  });
+}
--- public/signUp.js
+++ public/signUp.js
@@ -8,29 +8,53 @@ class user {
 
 var newUser = new user();
 
-function signUp() {
+async function signUp() {
  newUser.username = document.getElementById("username").value;
  newUser.email = document.getElementById("email").value;
  let psw = document.getElementById("psw").value;
  let pswRepeat = document.getElementById("psw-repeat").value;
- console.log(newUser.username);
- if (newUser.username == "" || newUser.email == "" || newUser.password == " ") {
+ let isValid = newUser.email.match(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/g);
+
+ let emailTaken = await takenEmail();
+
+ if (emailTaken) {
+  alert("This email is already in use. Please use another email.");
+ } else if (newUser.username == "" || newUser.email == "" || newUser.password == "") {
   alert("Please enter information in every box.");
+ } else if (!isValid) {
+  alert("Please enter a valid email address.");
  } else if (psw == pswRepeat) {
   newUser.password = psw;
+  console.log("it worked");
   postUser();
  } else {
   alert("Please enter the same password in both fields.");
  }
 }
-
+async function takenEmail() {
+ const app = firebase.app();
+ const db = firebase.firestore();
+ let emailTaken = false;
+ await db
+  .collection("users")
+  .get()
+  .then((querySnapshot) => {
+   querySnapshot.forEach((doc) => {
+    if (newUser.email == doc.data().email) {
+     console.log("yep");
+     emailTaken = true;
+    }
+   });
+  });
+ return emailTaken;
+}
 function postUser() {
+ const app = firebase.app();
+ console.log(app);
+ const db = firebase.firestore();
  db.collection("users").add({
-  name: post.name,
-  description: post.description,
-  skills: post.skills,
-  requirments: post.requirments,
+  username: newUser.username,
+  email: newUser.email,
+  password: newUser.password,
  });
-
- location.href = "../index.html";
 }
